b1d5e1e5859c3f9b3163fdaff96e96460ea62e86
gabicilfone <cilfonegabriel@gmail.com>
2023-05-10 20:50:51 -0300

    fetch completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import { fetchRandomGreeting } from './redux/greetingSlice';
+import Greeting from './components/greeting';
+
 function App() {
+  const dispatch = useDispatch();
+  const { greeting } = useSelector((state) => state.greeting);
+
+  useEffect(() => {
+    dispatch(fetchRandomGreeting());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Greeting greeting={greeting.message} />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
